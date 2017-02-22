fc8a36660c7d6d43de2c904ae27cfe65aa5bd3f2
Fernando Torres <fertorresmx@gmail.com>
2017-02-22 17:59:29 -0600

    Código listo hasta página 77. Eliminando alert's y validando existencia de usuario

--- ch02/scripts/validation.js
+++ ch02/scripts/validation.js
@@ -3,16 +3,42 @@ window.onload = initPage;
 function initPage() {
   document.getElementById("username").onblur = 
     checkUsername;
-    alert("Inside the initPage() function");
+    /*alert("Inside the initPage() function");*/
 }
 
 function checkUsername() {
   // get a request object and send
-  // it to the server
-  alert("Inside checkUsername()");
+  request = createRequest();
+  if (request == null)
+    alert("Unable to create request");
+  else
+  {
+    /*alert("Got the request object");*/
+    var theName = document.getElementById("username").value;
+    /*alert("Original name value: " + theName);*/
+    var username = escape(theName);
+    /*alert("Escaped name value: " + username);*/
+    var url = "checkName.php?username=" + username;
+    /*alert("URL: " + url);*/
+
+    request.onreadystatechange = showUsernameStatus;
+    request.open("GET", url, true);
+    request.send(null);
+  }
 }
 
 function showUsernameStatus() {
   //update the page to show whether
   // the user name is okay
+  if (request.readyState == 4) {
+    if (request.status == 200) {
+      if (request.responseText == "okay") {
+        // if it's okay, no error message to show
+      }
+      else {
+        // if there's a problem, we'll tell the user
+        alert("Sorry, that username is taken.");
+      }
+    }
+  }
 }
